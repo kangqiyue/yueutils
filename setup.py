59ccab92d3ccfcb59b39d808717531352ceaa7f1
kangqiyue <kangqiyue@westlake.edu.cn>
2023-07-26 22:01:55 +0800

    fix(*.py): add first commit

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
   name='yueutils',        # How you named your package folder (foo)
   packages=find_packages(),   # Chose the same as "name"
-  package_dir={'yueutils': 'src/yueutils'},
+  # package_dir={'yueutils': 'src/yueutils'},
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='afl-3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'common utils function',   # Give a short description about your library
   author = 'Alex',                   # Type in your name
-  install_requires=[["toml", "jsonlines"]],
+  install_requires=["toml", "jsonlines"],
   # classifiers=[
   #   'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
   #   'Intended Audience :: Developers',      # Define that your audience are developers
